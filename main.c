fecaed0e045756ae95413a80e9ec77298229c698
Victor Giordani <vgiordan@student.42lausanne.ch>
2022-11-30 17:29:33 +0100

    INIT

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vgiordan <vgiordan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 14:07:35 by vgiordan          #+#    #+#             */
-/*   Updated: 2022/11/30 17:15:59 by vgiordan         ###   ########.fr       */
+/*   Updated: 2022/11/30 17:27:57 by vgiordan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,76 @@ int mandelbrot_fonc(float a, float b, float c, void *mlx_ptr, void *win_ptr, int
     return (0);
 }
 
-void    fractal(void *mlx_ptr, void *win_ptr)
+void fractal(float left, float top, float xside, float yside)
+{
+    float xscale, yscale, zx, zy, cx, tempx, cy;
+    int x, y, i, j;
+    int maxx, maxy, count;
+  
+    // getting maximum value of x-axis of screen
+    maxx = 1920;
+  
+    // getting maximum value of y-axis of screen
+    maxy = 1080;
+  
+    // setting up the xscale and yscale
+    xscale = xside / maxx;
+    yscale = yside / maxy;
+  
+    // scanning every point in that rectangular area.
+    // Each point represents a Complex number (x + yi).
+    // Iterate that complex number
+    for (y = 1; y <= maxy - 1; y++) {
+        for (x = 1; x <= maxx - 1; x++)
+        {
+            // c_real
+            cx = x * xscale + left;
+  
+            // c_imaginary
+            cy = y * yscale + top;
+  
+            // z_real
+            zx = 0;
+  
+            // z_imaginary
+            zy = 0;
+            count = 0;
+  
+            // Calculate whether c(c_real + c_imaginary) belongs
+            // to the Mandelbrot set or not and draw a pixel
+            // at coordinates (x, y) accordingly
+            // If you reach the Maximum number of iterations
+            // and If the distance from the origin is
+            // greater than 2 exit the loop
+            while ((zx * zx + zy * zy < 50) && (count < 30))
+            {
+                // Calculate Mandelbrot function
+                // z = z*z + c where z is a complex number
+  
+                // tempx = z_real*_real - z_imaginary*z_imaginary + c_real
+                tempx = zx * zx - zy * zy + cx;
+  
+                // 2*z_real*z_imaginary + c_imaginary
+                zy = 2 * zx * zy + cy;
+  
+                // Updating z_real = tempx
+                zx = tempx;
+  
+                // Increment count
+                count = count + 1;
+            }
+  
+            // To display the created fractal
+             mlx_pixel_put(mlx_ptr, win_ptr, x, y, 2000000*count);
+        }
+    }
+}
+
+/*void    fractal(void *mlx_ptr, void *win_ptr)
 {
     int a;
     int b;
     int i;
-
     int x=0;
     int y=0;
 
@@ -72,9 +136,8 @@ void    fractal(void *mlx_ptr, void *win_ptr)
             y++;
         }
         x++;
-    }
-    
-}
+    }   
+}*/
 
 int main(int argc, char const *argv[])
 {
@@ -86,8 +149,8 @@ int main(int argc, char const *argv[])
     mlx_ptr = mlx_init();
     win_ptr = mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "FRACTAL =)");
     mlx_key_hook(win_ptr, key_is_pressed, (void *)0);
-    //fractal(mlx_ptr, win_ptr);
-    while (a > 0)
+    fractal(-1.75, -0.25, 0.25, 0.45);
+   /* while (a > 0)
     {   b = 1;
         while (b > 0)
         {
@@ -96,7 +159,7 @@ int main(int argc, char const *argv[])
             b -= 0.001;
         }
         a -= 0.001;
-    }
+    }*/
     
     mlx_loop(mlx_ptr);
     
